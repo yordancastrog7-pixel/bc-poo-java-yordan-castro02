73cd1e0aaf8f76b2d46fc0cdfe704a46f13adcb8
yordancastrog7-pixel <yordancastrog7@gmail.com>
2025-11-10 17:11:29 -0500

    Actualizo toda la semana 00

---
+++ SEMANA-00/2-practica/ejercicio-01-hola-mundo/MisDatosPersonales.java
@@ -0,0 +1,17 @@
+public class MisDatosPersonales {
+
+public static void main(String[] args) {
+ System.out.println("   MI INFORMACION");
+  
+        System.out.println("Nombre: [Eisin Yordan Castro Guerrero]");
+        System.out.println("Edad: [31]");
+        System.out.println("Ciudad: [Bogota]");
+        System.out.println("Ficha SENA: [3228970a]");
+
+
+
+    System.out.println("\n¡MisDatosPersonales!");
+
+
+}       
+}
